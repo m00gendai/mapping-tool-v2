6e22c66bfd45bfa0ef49cd68e8730c00d5ffff03
Marcel Weber <74364298+m00gendai@users.noreply.github.com>
2022-02-08 17:46:55 +0100

    Initial Commit

---
+++ Data/LS_SUB_JSON.js
@@ -0,0 +1,6 @@
+const SwitzerlandSub = [
+	{"type": "Feature",
+	"properties": {"name":"Switzerland FIR BDRY"}, 
+	"geometry": {
+		"type": "Polygon",
+	"coordinates":  [ [ [6.9553,47.2428],[7.8025,46.5097],[7.9914,46.5142],[8.2233,46.3308],[8.2233,46.3308],[7.9914,46.5142],[7.8025,46.5097],[6.9553,47.2428] ] ]}} ] 
