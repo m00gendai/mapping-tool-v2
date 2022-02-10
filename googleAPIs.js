79b39da42d1f102c941d640166f6685c98220c08
Marcel Weber <74364298+m00gendai@users.noreply.github.com>
2022-02-10 16:27:04 +0100

    Maptiler API Key

--- googleAPIs.js
+++ googleAPIs.js
@@ -1,1 +1,4 @@
+//These API keys are access restricted. Plz no shenanigans.
+
 export const MAPS_API_KEY = "AIzaSyCB-D8pZysZgYtXHl10EkmGJI6UWBuXTi0"
+export const MAPTILER_API_KEY = "NKhHZPzrFz5SSXwqLEH0"
