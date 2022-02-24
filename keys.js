657217e08cfacca6e34b18427941d33c23d4b233
Marcel Weber <74364298+m00gendai@users.noreply.github.com>
2022-02-24 18:07:44 +0100

    Key for French Geoservices

--- keys.js
+++ keys.js
@@ -2,3 +2,4 @@
 
 export const MAPS_API_KEY = "AIzaSyCB-D8pZysZgYtXHl10EkmGJI6UWBuXTi0"
 export const MAPTILER_API_KEY = "NKhHZPzrFz5SSXwqLEH0"
+export const IGN_FRANCE_API_KEY = "tf5vcp9r5dnrjkr4kpdcqvvs"
