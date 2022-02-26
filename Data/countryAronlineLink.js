33bfc10aa3fad2caff831e6ceddd3b4d1238c490
Marcel Weber <74364298+m00gendai@users.noreply.github.com>
2022-02-26 11:02:23 +0100

    Country Shapes Data and ICAO map

---
+++ Data/countryAronlineLink.js
@@ -0,0 +1,1024 @@
+// These are sorted by ISO A3 Code
+
+const countryAronlineLink = [
+    {
+    country: "Aruba",
+    icao: "TN"
+    },
+    {
+    country: "Afghanistan",
+    icao: "OA"
+    },
+    {
+    country: "Angola",
+    icao: "FN"
+    },
+    {
+    country: "Anguilla",
+    icao: "TQ"
+    },
+    {
+    country: "Albania",
+    icao: "LA"
+    },
+    {
+    country: "Aland",
+    icao: "EF"
+    },
+    {
+    country: "Andorra",
+    icao: "LF"
+    },
+    {
+    country: "United Arab Emirates",
+    icao: "OM"
+    },
+    {
+    country: "Argentina",
+    icao: "SA"
+    },
+    {
+    country: "Armenia",
+    icao: "UD"
+    },
+    {
+    country: "American Samoa",
+    icao: "NS"
+    },
+    {
+    country: "Antarctica",
+    icao: ""
+    },
+    {
+    country: "Ashmore and Cartier Islands",
+    icao: "YB"
+    },
+    {
+    country: "French Southern and Antarctic Lands",
+    icao: "LF"
+    },
+    {
+    country: "Antigua and Barbuda",
+    icao: "TA"
+    },
+    {
+    country: "Australia",
+    icao: "YB"
+    },
+    {
+    country: "Austria",
+    icao: "LO"
+    },
+    {
+    country: "Azerbaijan",
+    icao: "UB"
+    },
+    {
+    country: "Burundi",
+    icao: "HB"
+    },
+    {
+    country: "Belgium",
+    icao: "EB"
+    },
+    {
+    country: "Benin",
+    icao: "DB"
+    },
+    {
+    country: "Burkina Faso",
+    icao: "DF"
+    },
+    {
+    country: "Bangladesh",
+    icao: "VG"
+    },
+    {
+    country: "Bulgaria",
+    icao: "LB"
+    },
+    {
+    country: "Bahrain",
+    icao: "OB"
+    },
+    {
+    country: "The Bahamas",
+    icao: "MY"
+    },
+    {
+    country: "Bosnia and Herzegovina",
+    icao: "LQ"
+    },
+    {
+    country: "Bajo Nuevo Bank (Petrel Is.)",
+    icao: ""
+    },
+    {
+    country: "Saint Barthelemy",
+    icao: "TF"
+    },
+    {
+    country: "Belarus",
+    icao: "UM"
+    },
+    {
+    country: "Belize",
+    icao: "MZ"
+    },
+    {
+    country: "Bermuda",
+    icao: "TX"
+    },
+    {
+    country: "Bolivia",
+    icao: "SL"
+    },
+    {
+    country: "Brazil",
+    icao: "SB"
+    },
+    {
+    country: "Barbados",
+    icao: "TB"
+    },
+    {
+    country: "Brunei",
+    icao: "WB"
+    },
+    {
+    country: "Bhutan",
+    icao: "VQ"
+    },
+    {
+    country: "Botswana",
+    icao: "FB"
+    },
+    {
+    country: "Central African Republic",
+    icao: "FE"
+    },
+    {
+    country: "Canada",
+    icao: "CY"
+    },
+    {
+    country: "Switzerland",
+    icao: "LS"
+    },
+    {
+    country: "Chile",
+    icao: "SC"
+    },
+    {
+    country: "China",
+    icao: "ZB"
+    },
+    {
+    country: "Ivory Coast",
+    icao: "DI"
+    },
+    {
+    country: "Clipperton Island",
+    icao: "LF"
+    },
+    {
+    country: "Cameroon",
+    icao: "FK"
+    },
+    {
+    country: "Cyprus No Mans Area",
+    icao: ""
+    },
+    {
+    country: "Democratic Republic of the Congo",
+    icao: "FZ"
+    },
+    {
+    country: "Republic of Congo",
+    icao: "FC"
+    },
+    {
+    country: "Cook Islands",
+    icao: "NC"
+    },
+    {
+    country: "Colombia",
+    icao: "SK"
+    },
+    {
+    country: "Comoros",
+    icao: "FM"
+    },
+    {
+    country: "Cape Verde",
+    icao: "GV"
+    },
+    {
+    country: "Costa Rica",
+    icao: "MR"
+    },
+    {
+    country: "Coral Sea Islands",
+    icao: "YB"
+    },
+    {
+    country: "Cuba",
+    icao: "MU"
+    },
+    {
+    country: "Curaçao",
+    icao: "TN"
+    },
+    {
+    country: "Cayman Islands",
+    icao: "MW"
+    },
+    {
+    country: "Northern Cyprus",
+    icao: "LC"
+    },
+    {
+    country: "Cyprus",
+    icao: "LC"
+    },
+    {
+    country: "Czech Republic",
+    icao: "LK"
+    },
+    {
+    country: "Germany",
+    icao: "ED"
+    },
+    {
+    country: "Djibouti",
+    icao: "HD"
+    },
+    {
+    country: "Dominica",
+    icao: "TD"
+    },
+    {
+    country: "Denmark",
+    icao: "EK"
+    },
+    {
+    country: "Dominican Republic",
+    icao: "MD"
+    },
+    {
+    country: "Algeria",
+    icao: "DA"
+    },
+    {
+    country: "Ecuador",
+    icao: "SE"
+    },
+    {
+    country: "Egypt",
+    icao: "HE"
+    },
+    {
+    country: "Eritrea",
+    icao: "HA"
+    },
+    {
+    country: "Dhekelia Sovereign Base Area",
+    icao: "LC"
+    },
+    {
+    country: "Spain",
+    icao: "LE"
+    },
+    {
+    country: "Estonia",
+    icao: "EE"
+    },
+    {
+    country: "Ethiopia",
+    icao: "HA"
+    },
+    {
+    country: "Finland",
+    icao: "EF"
+    },
+    {
+    country: "Fiji",
+    icao: "NF"
+    },
+    {
+    country: "Falkland Islands",
+    icao: "SF"
+    },
+    {
+    country: "France",
+    icao: "LF"
+    },
+    {
+    country: "Faroe Islands",
+    icao: "EK"
+    },
+    {
+    country: "Federated States of Micronesia",
+    icao: "PT"
+    },
+    {
+    country: "Gabon",
+    icao: "FO"
+    },
+    {
+    country: "United Kingdom",
+    icao: "EG"
+    },
+    {
+    country: "Georgia",
+    icao: "UG"
+    },
+    {
+    country: "Guernsey",
+    icao: "EG"
+    },
+    {
+    country: "Ghana",
+    icao: "DG"
+    },
+    {
+    country: "Gibraltar",
+    icao: "LX"
+    },
+    {
+    country: "Guinea",
+    icao: "GU"
+    },
+    {
+    country: "Gambia",
+    icao: "GB"
+    },
+    {
+    country: "Guinea Bissau",
+    icao: "GG"
+    },
+    {
+    country: "Equatorial Guinea",
+    icao: "FG"
+    },
+    {
+    country: "Greece",
+    icao: "LG"
+    },
+    {
+    country: "Grenada",
+    icao: "TG"
+    },
+    {
+    country: "Greenland",
+    icao: "BG"
+    },
+    {
+    country: "Guatemala",
+    icao: "MG"
+    },
+    {
+    country: "Guam",
+    icao: "PG"
+    },
+    {
+    country: "Guyana",
+    icao: "SY"
+    },
+    {
+    country: "Hong Kong S.A.R.",
+    icao: "VH"
+    },
+    {
+    country: "Heard Island and McDonald Islands",
+    icao: ""
+    },
+    {
+    country: "Honduras",
+    icao: "MH"
+    },
+    {
+    country: "Croatia",
+    icao: "LD"
+    },
+    {
+    country: "Haiti",
+    icao: "MT"
+    },
+    {
+    country: "Hungary",
+    icao: "LH"
+    },
+    {
+    country: "Indonesia",
+    icao: "WI"
+    },
+    {
+    country: "Isle of Man",
+    icao: "EG"
+    },
+    {
+    country: "India",
+    icao: "VI"
+    },
+    {
+    country: "Indian Ocean Territories",
+    icao: "FJ"
+    },
+    {
+    country: "British Indian Ocean Territory",
+    icao: "FJ"
+    },
+    {
+    country: "Ireland",
+    icao: "EI"
+    },
+    {
+    country: "Iran",
+    icao: "OI"
+    },
+    {
+    country: "Iraq",
+    icao: "OR"
+    },
+    {
+    country: "Iceland",
+    icao: "BI"
+    },
+    {
+    country: "Israel",
+    icao: "LL"
+    },
+    {
+    country: "Italy",
+    icao: "LI"
+    },
+    {
+    country: "Jamaica",
+    icao: "MK"
+    },
+    {
+    country: "Jersey",
+    icao: "EG"
+    },
+    {
+    country: "Jordan",
+    icao: "OJ"
+    },
+    {
+    country: "Japan",
+    icao: "RJ"
+    },
+    {
+    country: "Baykonur Cosmodrome",
+    icao: "UA"
+    },
+    {
+    country: "Siachen Glacier",
+    icao: "VN"
+    },
+    {
+    country: "Kazakhstan",
+    icao: "UA"
+    },
+    {
+    country: "Kenya",
+    icao: "HK"
+    },
+    {
+    country: "Kyrgyzstan",
+    icao: "UC"
+    },
+    {
+    country: "Cambodia",
+    icao: "VD"
+    },
+    {
+    country: "Kiribati",
+    icao: "PC"
+    },
+    {
+    country: "Saint Kitts and Nevis",
+    icao: "TK"
+    },
+    {
+    country: "South Korea",
+    icao: "RK"
+    },
+    {
+    country: "Kosovo",
+    icao: "BK"
+    },
+    {
+    country: "Kuwait",
+    icao: "OK"
+    },
+    {
+    country: "Laos",
+    icao: "VL"
+    },
+    {
+    country: "Lebanon",
+    icao: "OL"
+    },
+    {
+    country: "Liberia",
+    icao: "GL"
+    },
+    {
+    country: "Libya",
+    icao: "HL"
+    },
+    {
+    country: "Saint Lucia",
+    icao: "TL"
+    },
+    {
+    country: "Liechtenstein",
+    icao: "LS"
+    },
+    {
+    country: "Sri Lanka",
+    icao: "VC"
+    },
+    {
+    country: "Lesotho",
+    icao: "FX"
+    },
+    {
+    country: "Lithuania",
+    icao: "EY"
+    },
+    {
+    country: "Luxembourg",
+    icao: "EL"
+    },
+    {
+    country: "Latvia",
+    icao: "EV"
+    },
+    {
+    country: "Macao S.A.R",
+    icao: "VM"
+    },
+    {
+    country: "Saint Martin",
+    icao: "TF"
+    },
+    {
+    country: "Morocco",
+    icao: "GM"
+    },
+    {
+    country: "Monaco",
+    icao: "LN"
+    },
+    {
+    country: "Moldova",
+    icao: "LU"
+    },
+    {
+    country: "Madagascar",
+    icao: "FM"
+    },
+    {
+    country: "Maldives",
+    icao: "VR"
+    },
+    {
+    country: "Mexico",
+    icao: "MM"
+    },
+    {
+    country: "Marshall Islands",
+    icao: "PK"
+    },
+    {
+    country: "Macedonia",
+    icao: "LW"
+    },
+    {
+    country: "Mali",
+    icao: "GA"
+    },
+    {
+    country: "Malta",
+    icao: "LM"
+    },
+    {
+    country: "Myanmar",
+    icao: "VY"
+    },
+    {
+    country: "Montenegro",
+    icao: "LY"
+    },
+    {
+    country: "Mongolia",
+    icao: "ZM"
+    },
+    {
+    country: "Northern Mariana Islands",
+    icao: "PG"
+    },
+    {
+    country: "Mozambique",
+    icao: "FQ"
+    },
+    {
+    country: "Mauritania",
+    icao: "GQ"
+    },
+    {
+    country: "Montserrat",
+    icao: "TR"
+    },
+    {
+    country: "Mauritius",
+    icao: "FI"
+    },
+    {
+    country: "Malawi",
+    icao: "FW"
+    },
+    {
+    country: "Malaysia",
+    icao: "WM"
+    },
+    {
+    country: "Namibia",
+    icao: "FY"
+    },
+    {
+    country: "New Caledonia",
+    icao: "NW"
+    },
+    {
+    country: "Niger",
+    icao: "DR"
+    },
+    {
+    country: "Norfolk Island",
+    icao: "AS"
+    },
+    {
+    country: "Nigeria",
+    icao: "DN"
+    },
+    {
+    country: "Nicaragua",
+    icao: "MN"
+    },
+    {
+    country: "Niue",
+    icao: "NI"
+    },
+    {
+    country: "Netherlands",
+    icao: "EH"
+    },
+    {
+    country: "Norway",
+    icao: "EN"
+    },
+    {
+    country: "Nepal",
+    icao: "VN"
+    },
+    {
+    country: "Nauru",
+    icao: "AN"
+    },
+    {
+    country: "New Zealand",
+    icao: "NZ"
+    },
+    {
+    country: "Oman",
+    icao: "OO"
+    },
+    {
+    country: "Pakistan",
+    icao: "OP"
+    },
+    {
+    country: "Panama",
+    icao: "MP"
+    },
+    {
+    country: "Pitcairn Islands",
+    icao: "NT"
+    },
+    {
+    country: "Peru",
+    icao: "SP"
+    },
+    {
+    country: "Spratly Islands",
+    icao: ""
+    },
+    {
+    country: "Philippines",
+    icao: "RP"
+    },
+    {
+    country: "Palau",
+    icao: "PT"
+    },
+    {
+    country: "Papua New Guinea",
+    icao: "AY"
+    },
+    {
+    country: "Poland",
+    icao: "EP"
+    },
+    {
+    country: "Puerto Rico",
+    icao: "TJ"
+    },
+    {
+    country: "North Korea",
+    icao: "ZK"
+    },
+    {
+    country: "Portugal",
+    icao: "LP"
+    },
+    {
+    country: "Paraguay",
+    icao: "SG"
+    },
+    {
+    country: "Palestine",
+    icao: "LV"
+    },
+    {
+    country: "French Polynesia",
+    icao: "NT"
+    },
+    {
+    country: "Qatar",
+    icao: "OT"
+    },
+    {
+    country: "Romania",
+    icao: "LR"
+    },
+    {
+    country: "Russia",
+    icao: "UU"
+    },
+    {
+    country: "Rwanda",
+    icao: "HR"
+    },
+    {
+    country: "Western Sahara",
+    icao: "GS"
+    },
+    {
+    country: "Saudi Arabia",
+    icao: "OE"
+    },
+    {
+    country: "Scarborough Reef",
+    icao: ""
+    },
+    {
+    country: "Sudan",
+    icao: "HS"
+    },
+    {
+    country: "South Sudan",
+    icao: "HS"
+    },
+    {
+    country: "Senegal",
+    icao: "GO"
+    },
+    {
+    country: "Serranilla Bank",
+    icao: ""
+    },
+    {
+    country: "Singapore",
+    icao: "WS"
+    },
+    {
+    country: "South Georgia and South Sandwich Islands",
+    icao: "EG"
+    },
+    {
+    country: "Saint Helena",
+    icao: "FH"
+    },
+    {
+    country: "Solomon Islands",
+    icao: "AG"
+    },
+    {
+    country: "Sierra Leone",
+    icao: "GF"
+    },
+    {
+    country: "El Salvador",
+    icao: "MS"
+    },
+    {
+    country: "San Marino",
+    icao: "LI"
+    },
+    {
+    country: "Somaliland",
+    icao: "HC"
+    },
+    {
+    country: "Somalia",
+    icao: "HC"
+    },
+    {
+    country: "Saint Pierre and Miquelon",
+    icao: "LF"
+    },
+    {
+    country: "Republic of Serbia",
+    icao: "LY"
+    },
+    {
+    country: "Sao Tome and Principe",
+    icao: "FP"
+    },
+    {
+    country: "Suriname",
+    icao: "SM"
+    },
+    {
+    country: "Slovakia",
+    icao: "LZ"
+    },
+    {
+    country: "Slovenia",
+    icao: "LJ"
+    },
+    {
+    country: "Sweden",
+    icao: "ES"
+    },
+    {
+    country: "Swaziland",
+    icao: "FD"
+    },
+    {
+    country: "Sint Maarten",
+    icao: "TN"
+    },
+    {
+    country: "Seychelles",
+    icao: "FS"
+    },
+    {
+    country: "Syria",
+    icao: "OS"
+    },
+    {
+    country: "Turks and Caicos Islands",
+    icao: "MB"
+    },
+    {
+    country: "Chad",
+    icao: "FT"
+    },
+    {
+    country: "Togo",
+    icao: "DX"
+    },
+    {
+    country: "Thailand",
+    icao: "VT"
+    },
+    {
+    country: "Tajikistan",
+    icao: "UT"
+    },
+    {
+    country: "Turkmenistan",
+    icao: "UT"
+    },
+    {
+    country: "East Timor",
+    icao: "WP"
+    },
+    {
+    country: "Tonga",
+    icao: "NF"
+    },
+    {
+    country: "Trinidad and Tobago",
+    icao: "TT"
+    },
+    {
+    country: "Tunisia",
+    icao: "DT"
+    },
+    {
+    country: "Turkey",
+    icao: "LT"
+    },
+    {
+    country: "Tuvalu",
+    icao: "NG"
+    },
+    {
+    country: "Taiwan",
+    icao: "RC"
+    },
+    {
+    country: "United Republic of Tanzania",
+    icao: "HT"
+    },
+    {
+    country: "Uganda",
+    icao: "HU"
+    },
+    {
+    country: "Ukraine",
+    icao: "UK"
+    },
+    {
+    country: "United States Minor Outlying Islands",
+    icao: "KD"
+    },
+    {
+    country: "Uruguay",
+    icao: "SU"
+    },
+    {
+    country: "United States of America",
+    icao: "KD"
+    },
+    {
+    country: "US Naval Base Guantanamo Bay",
+    icao: "KD"
+    },
+    {
+    country: "Uzbekistan",
+    icao: "UT"
+    },
+    {
+    country: "Vatican",
+    icao: "LI"
+    },
+    {
+    country: "Saint Vincent and the Grenadines",
+    icao: "TV"
+    },
+    {
+    country: "Venezuela",
+    icao: "SV"
+    },
+    {
+    country: "British Virgin Islands",
+    icao: "TU"
+    },
+    {
+    country: "United States Virgin Islands",
+    icao: "TI"
+    },
+    {
+    country: "Vietnam",
+    icao: "VV"
+    },
+    {
+    country: "Vanuatu",
+    icao: "NV"
+    },
+    {
+    country: "Wallis and Futuna",
+    icao: "NL"
+    },
+    {
+    country: "Akrotiri Sovereign Base Area",
+    icao: "LC"
+    },
+    {
+    country: "Samoa",
+    icao: "NS"
+    },
+    {
+    country: "Yemen",
+    icao: "OY"
+    },
+    {
+    country: "South Africa",
+    icao: "FA"
+    },
+    {
+    country: "Zambia",
+    icao: "FL"
+    },
+    {
+    country: "Zimbabwe",
+    icao: "FV"
+    },
+]
